644fd70b47d6f7c69a809fb7d6273ad0c8b17922
akki <akshatharao92@gmail.com>
2025-07-31 13:37:12 +0000

    added vulnerability for codeql check

---
+++ vuln_demo.py
@@ -0,0 +1,17 @@
+import sqlite3
+import sys
+
+def get_user_data(username):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+
+    # Vulnerable: unsanitized input directly in query
+    query = "SELECT * FROM users WHERE name = '" + username + "';"
+    cursor.execute(query)
+
+    for row in cursor.fetchall():
+        print(row)
+
+if __name__ == "__main__":
+    get_user_data(sys.argv[1])
+
